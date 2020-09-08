8e32d7b4e7f8d0d9888ebd37ff13ba42a898f349
GabrielaCorona19 <70930147+GabrielaCorona19@users.noreply.github.com>
2020-09-07 22:35:10 -0500

    Create Hilo2.java

---
+++ Hilo2.java
@@ -0,0 +1,56 @@
+package practica1_20084;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextArea;
+
+public class Hilo2 extends Thread{
+    private JTextArea area;
+    public boolean suspender,pausar;
+    public Hilo2(JTextArea area){
+        this.area=area;
+        this.suspender=false;
+        this.pausar=false;
+    }
+    public void run(){
+        int i=0;
+            while(true){
+                if(suspender==false && pausar==false){
+                    area.append(i + "\n");
+                    i++;
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Hilo1.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }else{
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Hilo1.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                if(suspender==true && pausar==true){
+                    area.append("Hilo Detenido");
+                    break;
+                }
+            }
+    }
+    
+    //Pausar el hilo
+    public void pausarhilo(){
+        this.pausar=false;
+        //lo siguiente garantiza que un hilo suspendido puede detenerse.
+        this.suspender=true;
+    }
+
+    //Renaudar un hilo
+   public void renaudarhilo(){
+       this.pausar=false;
+        this.suspender=false;
+    }
+   
+   public void deterhilo(){
+       this.pausar=true;
+       this.suspender=true;
+   }
+}
